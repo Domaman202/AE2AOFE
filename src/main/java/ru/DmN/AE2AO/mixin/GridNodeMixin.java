eaec3890bd7953e523db6a611c6bd8c107f1bff2
Domaman202 <vip.domaman@mail.ru>
2021-10-06 08:16:37 +0700

    .

---
+++ src/main/java/ru/DmN/AE2AO/mixin/GridNodeMixin.java
@@ -0,0 +1,21 @@
+package ru.DmN.AE2AO.mixin;
+
+import appeng.me.GridNode;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import ru.DmN.AE2AO.Main;
+
+@Mixin(value = GridNode.class, remap = false)
+public class GridNodeMixin {
+    @Shadow private int usedChannels;
+
+    /**
+     * @author DomamaN202
+     * @reason Add system turn off channels
+     */
+    @Overwrite private int getUsedChannels()
+    {
+        return Main.lc.DisableChannels ? 1 : usedChannels;
+    }
+}
