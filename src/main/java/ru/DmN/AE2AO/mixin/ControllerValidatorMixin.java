248ff7cc5eedccbcd9d461d5964edd697e811644
Domaman202 <vip.domaman@mail.ru>
2022-01-24 20:09:07 +0700

    negr

--- src/main/java/ru/DmN/AE2AO/mixin/ControllerValidatorMixin.java
+++ src/main/java/ru/DmN/AE2AO/mixin/ControllerValidatorMixin.java
@@ -1,50 +1,97 @@
 package ru.DmN.AE2AO.mixin;
 
-import appeng.api.networking.IGridHost;
 import appeng.api.networking.IGridNode;
+import appeng.api.networking.pathing.ControllerState;
+import appeng.blockentity.networking.ControllerBlockEntity;
 import appeng.me.pathfinding.ControllerValidator;
-import appeng.tile.networking.ControllerTileEntity;
+import net.minecraft.core.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import ru.DmN.AE2AO.Main;
 
+import java.util.Collection;
+
 @Mixin(value = ControllerValidator.class, remap = false)
 public class ControllerValidatorMixin {
-    @Shadow private boolean isValid;
-    @Shadow private int found;
-    @Shadow private int minX;
-    @Shadow private int minY;
-    @Shadow private int minZ;
-    @Shadow private int maxX;
-    @Shadow private int maxY;
-    @Shadow private int maxZ;
+    @Shadow
+    private boolean valid;
+    @Shadow
+    private int found;
+    @Shadow
+    private int minX;
+    @Shadow
+    private int minY;
+    @Shadow
+    private int minZ;
+    @Shadow
+    private int maxX;
+    @Shadow
+    private int maxY;
+    @Shadow
+    private int maxZ;
+
+    @Shadow
+    private static boolean hasControllerCross(Collection<ControllerBlockEntity> controllers) {
+        return false;
+    }
 
     /**
      * @author DomamaN202
      * @reason Adding a size customization
      */
-    @Overwrite public boolean visitNode(IGridNode n) {
-        IGridHost h = n.getMachine();
+    @Overwrite
+    public boolean visitNode(IGridNode n) {
+        if (valid && n.getOwner() instanceof ControllerBlockEntity c) {
+            BlockPos pos = c.getBlockPos();
 
-        if (isValid && h instanceof ControllerTileEntity) {
-//            BlockPos pos = ((ControllerTileEntity) h).getPos();
-//
-//            minX = Math.min(pos.getX(), minX);
-//            maxX = Math.max(pos.getX(), maxX);
-//            minY = Math.min(pos.getY(), minY);
-//            maxY = Math.max(pos.getY(), maxY);
-//            minZ = Math.min(pos.getZ(), minZ);
-//            maxZ = Math.max(pos.getZ(), maxZ);
+            minX = Math.min(pos.getX(), minX);
+            maxX = Math.max(pos.getX(), maxX);
+            minY = Math.min(pos.getY(), minY);
+            maxY = Math.max(pos.getY(), maxY);
+            minZ = Math.min(pos.getZ(), minZ);
+            maxZ = Math.max(pos.getZ(), maxZ);
 
             if (maxX - minX < Main.lc.Max_X && maxY - minY < Main.lc.Max_Y && maxZ - minZ < Main.lc.Max_Z) {
                 this.found++;
                 return true;
             }
 
-            isValid = false;
+            valid = false;
         }
 
         return false;
+    }
+
+    /**
+     * @author DomamaN202
+     */
+    @Overwrite
+    public static ControllerState calculateState(Collection<ControllerBlockEntity> controllers) throws Throwable {
+        if (controllers.isEmpty()) {
+            return ControllerState.NO_CONTROLLER;
+        }
+
+        var startingController = controllers.iterator().next();
+        var startingNode = startingController.getGridNode();
+        if (startingNode == null) {
+            return ControllerState.CONTROLLER_CONFLICT;
+        }
+
+        var constructor = ControllerValidator.class.getDeclaredConstructor(BlockPos.class);
+        constructor.setAccessible(true);
+        var cv = constructor.newInstance(startingController.getBlockPos());
+        startingNode.beginVisit(cv);
+
+        if (!cv.isValid())
+            return ControllerState.CONTROLLER_CONFLICT;
+
+        if (cv.getFound() != controllers.size() && Main.lc.ControllerLimits)
+            return ControllerState.CONTROLLER_CONFLICT;
+
+        if (hasControllerCross(controllers))
+            return ControllerState.CONTROLLER_CONFLICT;
+
+        return ControllerState.CONTROLLER_ONLINE;
     }
 }
