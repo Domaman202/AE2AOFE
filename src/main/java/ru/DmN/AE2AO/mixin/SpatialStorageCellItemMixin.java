2bdd5aa475c05b35d648f726c3e8b5828573e015
PashkovD <dimon2006763@gmail.com>
2022-10-29 23:27:35 +0500

    Returned cell fire resistance setting

---
+++ src/main/java/ru/DmN/AE2AO/mixin/SpatialStorageCellItemMixin.java
@@ -0,0 +1,13 @@
+package ru.DmN.AE2AO.mixin;
+
+import appeng.items.storage.SpatialStorageCellItem;
+import org.spongepowered.asm.mixin.Mixin;
+import ru.DmN.AE2AO.AE2AOMain;
+
+@Mixin(value = SpatialStorageCellItem.class, remap = false)
+public class SpatialStorageCellItemMixin {
+    boolean isFireResistant;
+    public boolean isFireResistant() {
+        return AE2AOMain.config.SCFD || this.isFireResistant;
+    }
+}
