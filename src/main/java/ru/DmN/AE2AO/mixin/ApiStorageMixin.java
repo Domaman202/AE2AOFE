b623c5c4452940915c0ab83a8a6426a538c45750
PashkovD <dimon2006763@gmail.com>
2022-11-06 20:32:50 +0500

    Added ItemsPerByte

--- src/main/java/ru/DmN/AE2AO/mixin/ApiStorageMixin.java
+++ src/main/java/ru/DmN/AE2AO/mixin/ApiStorageMixin.java
@@ -4,6 +4,7 @@ package ru.DmN.AE2AO.mixin;
 import appeng.api.storage.IStorageChannel;
 import appeng.api.storage.IStorageHelper;
 import appeng.api.storage.channels.IFluidStorageChannel;
+import appeng.api.storage.channels.IItemStorageChannel;
 import appeng.core.api.ApiStorage;
 import com.google.common.collect.ClassToInstanceMap;
 import org.spongepowered.asm.mixin.Final;
@@ -13,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import ru.DmN.AE2AO.AE2AOFluidStorageChannel;
+import ru.DmN.AE2AO.AE2AOItemStorageChannel;
 
 @Mixin(value = ApiStorage.class, remap = false)
 public abstract class ApiStorageMixin implements IStorageHelper {
@@ -22,6 +24,7 @@ public abstract class ApiStorageMixin implements IStorageHelper {
 
     @Inject(at = @At("RETURN"), method = "<init>")
     public void injectInit(CallbackInfo ci) {
+        this.channels.putInstance(IItemStorageChannel.class, new AE2AOItemStorageChannel());
         this.channels.putInstance(IFluidStorageChannel.class, new AE2AOFluidStorageChannel());
     }
 
